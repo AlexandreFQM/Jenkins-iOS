0f83c7a04009ea8c8a7c2dd8cb5fada89912ac33
Alexandre Furquim <>
2019-01-16 14:14:10 -0200

    asd

--- xpto/xpto/ViewController.swift
+++ xpto/xpto/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  //Truths
+  private let truths = [
+    "What are you most self-conscious about?",
+    "What would you do if you were the sex for a month?",
+    "What is the most expensive thing you have stolen?",
+    "What is the most childish thing you still do?",
+    "Have you ever let someone take the blame for something you did?"
+  ]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
