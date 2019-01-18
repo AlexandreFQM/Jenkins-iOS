c27c8744a7b2f7aba211a35d5bbd858e81fb8ebc
Alexandre Furquim <>
2019-01-18 13:07:47 -0200

    Novo App com Test

--- Pods/Target Support Files/Pods-xpto/Pods-xpto-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_xpto : NSObject
-@end
-@implementation PodsDummy_Pods_xpto
-@end
---
+++ xpto/xpto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xpto
+//
+//  Created by Alexandre Furquim on 18/01/19.
+//  Copyright © 2019 Alexandre Furquim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
